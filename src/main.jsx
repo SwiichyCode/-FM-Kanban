7e8ab0fb2a54ff389c8631ca28ffe14e15708123
Swiichy <adlpromail@gmail.com>
2022-11-24 22:14:12 +0100

    feat: add README.md

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
 import { RecoilRoot } from "recoil";
 import { createRoot } from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { App } from "./routes/App";
 import { GlobalStyle } from "./styles/GlobalStyle";
 
