0939e5a4bc6b722e57e0c1e771bd50b608f6f2b7
Swiichy <adlpromail@gmail.com>
2022-11-24 22:26:59 +0100

    .

--- src/libs/layouts/Sidebar/index.jsx
+++ src/libs/layouts/Sidebar/index.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import styled from "styled-components";
 import { SidebarLogo } from "./SidebarLogo";
 import { SidebarNav } from "./SidebarNav";
-import { SidebarContainer } from "./style";
 
 export const Sidebar = () => {
   return (
