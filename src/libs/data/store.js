1504ee99bfc197f20f5c4365fc2fcbf139b4ebe6
Swiichy <adlpromail@gmail.com>
2022-11-25 16:15:12 +0100

    feat: add recoilPersist for stop data in localStorage apply directly on the atom

--- src/libs/data/store.js
+++ src/libs/data/store.js
@@ -5,11 +5,14 @@ import {
   useRecoilState,
   useRecoilValue,
 } from "recoil";
-
+import { recoilPersist } from "recoil-persist";
 import data from "./data.json";
+
+const { persistAtom } = recoilPersist();
 
 // Store all data
 export const boardState = atom({
   key: "boardState",
   default: data,
+  effects_UNSTABLE: [persistAtom],
 });
