78c97b538b5505d1ce8a7408fe3274ce09c1f757
Swiichy <adlpromail@gmail.com>
2022-11-26 19:08:11 +0100

    feat: add skeleton of ToggleSwitch compenent

--- src/libs/layouts/Board/index.jsx
+++ src/libs/layouts/Board/index.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { useParams } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import { boardState } from "../../data/store";
 import { urlSplit } from "../../utils/urlSplit";
+import { ToggleSwitch } from "../../components/ToggleSwitch";
 
 export const Board = () => {
   let { name } = useParams();
   const boardData = useRecoilValue(boardState);
+  const [value, setValue] = useState(false);
 
   return (
     <BoardContainer>
@@ -16,6 +18,7 @@ export const Board = () => {
         .map((item, index) => (
           <p key={index}>{item.name}</p>
         ))}
+      <ToggleSwitch isOn={value} handleToggle={() => setValue(!value)} />
     </BoardContainer>
   );
 };
