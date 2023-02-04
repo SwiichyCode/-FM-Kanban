bdda6ce604a0381b6500413147426f131ee097a1
Swiichy <adlpromail@gmail.com>
2023-02-04 20:11:42 +0100

    feat: Finalization of the components implementation in the modal (edit task)

--- src/libs/layouts/modal/EditTask.jsx
+++ src/libs/layouts/modal/EditTask.jsx
@@ -1,12 +1,23 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { Input } from "../../components/Form/Input";
+import { Select } from "../../components/Form/Select";
 import { InputGeneratorTest } from "../../components/Form/Test/InputGeneratorTest";
 import { TextArea } from "../../components/Form/TextArea";
 import { LayoutModal } from "../../components/Wrapper/LayoutModal";
+import { useParams } from "react-router-dom";
+import useDashboardStore from "../../../store/dashboardStore";
+import { Button } from "../../components/Button";
 
 export const EditTask = ({ openEdit, setOpenEdit, item }) => {
   const [inputFields, setInputFields] = useState([]);
+  const { id } = useParams();
+  const board = useDashboardStore((state) => state.dashboard);
+  const currentBoard = board.find((board) => board.id === id);
+
+  const [status, setStatus] = useState(
+    currentBoard.columns.length > 0 ? currentBoard.columns[0].id : null
+  );
 
   useEffect(() => {
     setInputFields(item.subtasks);
@@ -36,8 +47,16 @@ export const EditTask = ({ openEdit, setOpenEdit, item }) => {
 
           <InputGeneratorTest
             inputFields={item.subtasks}
-            labelText={"azdzad"}
+            labelText={"Subtasks"}
           />
+
+          <Select
+            label="Status"
+            columns={currentBoard.columns}
+            setStatus={setStatus}
+          />
+
+          <Button theme="primary" text="Save Changes" type="submit" />
         </Form>
       </LayoutModal>
     </Container>
