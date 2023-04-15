cd388009b6c48881214df338986a8ac816dc2c03
Swiichy <adlpromail@gmail.com>
2023-04-15 15:00:37 +0200

    .

---
+++ src/main.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { App } from "./App";
+import "./styles/index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/:id",
+        element: <App />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root") as HTMLElement).render(
+  <RouterProvider router={router} />
+);
